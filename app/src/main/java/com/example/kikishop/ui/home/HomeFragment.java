10e2c8459a68668d3981247192072c1f16a60e46
larfin <adrian.zajac.it@wp.pl>
2025-08-06 14:41:22 +0200

    check point 1
    login view
    home,dashboard, notification view
    
    with out any addition or view layout finished

---
+++ app/src/main/java/com/example/kikishop/ui/home/HomeFragment.java
@@ -0,0 +1,50 @@
+package com.example.kikishop.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.kikishop.databinding.FragmentHomeBinding;
+import com.example.kikishop.ui.order.OrdersActivity;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        binding.ordersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), OrdersActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
